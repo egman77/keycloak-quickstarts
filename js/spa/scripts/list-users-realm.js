c3cdb6c77fa71aade2bdc63fff84b225f3b10d6e
hdh <hdh@cvoit.com>
2023-10-23 10:40:55 +0000

    更新脚本

--- js/spa/scripts/list-users-realm.js
+++ js/spa/scripts/list-users-realm.js
@@ -1,6 +1,13 @@
 import { readFileSync } from 'node:fs';
 import adminClient from './keycloak-admin-client.js';
+//import KcAdminClient from '@keycloak/keycloak-admin-client';
+//import config from '../config/config.js';
 
+//const adminClient=new KcAdminClient(config.adminClient);
+//await adminClient.auth(config.adminClient);
+
+
+//const users=await adminClient.users;
 const users=await adminClient.users.find({first :0 , max : 10 });
 
 console.log(users);
